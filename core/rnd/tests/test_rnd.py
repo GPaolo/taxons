1eb6123b3e2d480344557fa1ab02751205011d3c
Giuseppe Paolo <giuseppe.paolo93@gmail.com>
2019-01-16 10:13:59 +0100

    Updated rnd test

--- core/rnd/tests/test_rnd.py
+++ core/rnd/tests/test_rnd.py
@@ -5,16 +5,16 @@ torch.manual_seed(7)
 
 def test_surprise():
   device = torch.device('cpu')
-  net = rnd.RND(6, 2, device)
-  x = torch.ones((1,6))
+  net = rnd.RND(6, 2, 5, device)
+  x = torch.ones(1, 3, 6)
   surprise = net(x)
   # Surprise needs to be different than 0 at the beginning, otherwise we cannot use it.
   assert surprise.item() != 0, 'Surprise value is 0!'
 
 def test_training():
   device = torch.device('cpu')
-  net = rnd.RND(6, 2, device)
-  x = torch.ones((1, 6))
+  net = rnd.RND(6, 2, 5, device)
+  x = torch.ones(5, 3, 6)
 
   loss1 = net.training_step(x)
   loss2 = net.training_step(x)
