c5eb5c9f5fd29c60a0b0664969b70bb807c78b30
Giuseppe Paolo <giuseppe.paolo93@gmail.com>
2019-01-16 09:55:44 +0100

    Updated agents test

--- core/qd/tests/test_agents.py
+++ core/qd/tests/test_agents.py
@@ -4,11 +4,10 @@ import numpy as np
 np.random.seed(7)
 
 def test_base_agent():
-  agent = agents.BaseAgent(mutation_distr=None)
-  assert np.isclose(agent.mutation_operator(), 0.33810514076007125), 'Default mutation operator does not work'
-
-  agent = agents.BaseAgent(mutation_distr=np.random.randint)
-  assert  agent.mutation_operator(10) == 9, 'Cannot pass new mutation operator.'
+  try:
+    agent = agents.BaseAgent(mutation_distr=None)
+  except TypeError:
+    pass
 
 def test_neural_agent():
   shapes = {'input_shape': 3, 'output_shape':2}
@@ -22,7 +21,15 @@ def test_neural_agent():
   except:
     raise Exception('Call function not working.')
 
-  assert np.shape(result) == (1,2), 'Wrong output shape.'
-  assert np.allclose(result, [[-0.70445828, -0.94089774]]), 'Wrong output.'
+  assert np.shape(result) == (1,2), "Wrong output shape."
+  assert np.allclose(result, [[0.64340802, -0.43566705]]), 'Wrong output.'
 
+def test_mutation_operator():
+  shapes = {'input_shape': 3, 'output_shape': 2}
+  agent = agents.FFNeuralAgent(shapes)
+
+  assert np.isclose(agent.mutation_operator(), -0.023661300383801844), "Default mutation operator does not work"
+
+  agent = agents.FFNeuralAgent(shapes, mutation_distr=np.random.randint)
+  assert agent.mutation_operator(10) == 2, 'Cannot pass new mutation operator.'
 
