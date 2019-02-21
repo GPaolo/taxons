73ecf76ba2268a8085dd77f1dfb3a51ec4749581
Giuseppe Paolo <giuseppe.paolo93@gmail.com>
2019-02-21 15:16:16 +0100

    Might have found a good AE configuration

--- test.py
+++ test.py
@@ -16,13 +16,19 @@ class AutoEncoder(nn.Module):
 
     self.subsample = nn.AvgPool2d(8).cuda(self.device) # 600 -> 75
 
-    self.encoder = nn.Sequential(nn.Conv2d(in_channels=3, out_channels=8, kernel_size=5, stride=2, padding=1), nn.ReLU(), # 75 -> 37
-                                 nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=2), nn.ReLU(), # 37 -> 17
-                                 nn.Conv2d(in_channels=16, out_channels=8, kernel_size=3, stride=1), nn.ReLU()).cuda(self.device) # 17 -> 8
+    # self.encoder = nn.Sequential(nn.Conv2d(in_channels=3, out_channels=8, kernel_size=5, stride=2, padding=1), nn.ReLU(), # 75 -> 37
+    #                              nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=2), nn.ReLU(), # 37 -> 17
+    #                              nn.Conv2d(in_channels=16, out_channels=4, kernel_size=3, stride=1), nn.ReLU()).cuda(self.device) # 17 -> 8
+    #
+    # self.decoder = nn.Sequential(nn.ConvTranspose2d(in_channels=4, out_channels=8, kernel_size=3, stride=1), nn.ReLU(), # 8 -> 17
+    #                              nn.ConvTranspose2d(in_channels=8, out_channels=4, kernel_size=3, stride=2), nn.ReLU(), # 17 -> 35
+    #                              nn.ConvTranspose2d(in_channels=4, out_channels=3, kernel_size=3, stride=2), nn.ReLU()).cuda(self.device) # 35 -> 75
 
-    self.decoder = nn.Sequential(nn.ConvTranspose2d(in_channels=8, out_channels=16, kernel_size=3, stride=1), nn.ReLU(), # 8 -> 17
-                                 nn.ConvTranspose2d(in_channels=16, out_channels=8, kernel_size=3, stride=2), nn.ReLU(), # 17 -> 35
-                                 nn.ConvTranspose2d(in_channels=8, out_channels=3, kernel_size=3, stride=2), nn.ReLU()).cuda(self.device) # 35 -> 75
+    self.encoder = nn.Sequential(nn.Conv2d(in_channels=3, out_channels=4, kernel_size=7, stride=2), nn.ReLU(), # 75 -> 35
+                                 nn.Conv2d(in_channels=4, out_channels=4, kernel_size=5, stride=3), nn.ReLU()).cuda(self.device) # 35 -> 11
+
+    self.decoder = nn.Sequential(nn.ConvTranspose2d(in_channels=4, out_channels=4, kernel_size=5, stride=3), nn.ReLU(),
+                                 nn.ConvTranspose2d(in_channels=4, out_channels=3, kernel_size=7, stride=2), nn.ReLU()).cuda(self.device)
 
     self.criterion = nn.MSELoss().cuda(self.device)
     self.learning_rate = 0.001
@@ -41,7 +47,7 @@ class AutoEncoder(nn.Module):
     self.optimizer.zero_grad()
 
     y = self.forward(x)
-    x = self.subsample(x/255)
+    x = self.subsample(x)
     loss = self.criterion(x, y)
     loss.backward()
     # torch.nn.utils.clip_grad_norm_(self.parameters(), 0.1)
@@ -50,42 +56,57 @@ class AutoEncoder(nn.Module):
 
 
 if __name__ == '__main__':
-  # env = gym.make('Billiard-v0')
-  # state = []
-  #
-  # import matplotlib
-  # matplotlib.use('agg')
-  # for i in range(16):
-  #   env.reset()
-  #   state.append(env.render(rendered=False))
-  #
-  # state = np.array(state)
-  #
-
   import matplotlib.pyplot as plt
   with open('/home/giuseppe/src/rnd_qd/input_img.npy', 'rb') as f:
     x = np.load(f)
 
-  # plt.imshow(x/255)
-  # plt.show()
+  norm = False
+  factor = 1
+  if norm:
+    factor = 255
 
   net = AutoEncoder()
-  x = torch.Tensor(x).permute(0, 3, 1, 2).cuda(net.device)
-  net(x)
+  x = torch.Tensor(x/factor).permute(0, 3, 1, 2).cuda(net.device)
+  test = x[15:16]
+  train = x[0:15]
 
   for k in range(5000):
-    net.train_ae(x)
-
-  b = net(x[0:1])
+    net.train_ae(train)
 
 
+  fig, ax = plt.subplots(4, 4)
 
 
-  # print(loss)
+
+  for i in range(4):
+    for j in range(4):
+      k = i+j
+      b = net(x[k:k+1])
+      a = b[0]
+      a = a.permute(1, 2, 0)
+      a = a.cpu().data.numpy()
+      if not norm:
+        a = a.astype(np.int)
+
+      ax[i, j].imshow(a)
+      # plt.imshow(a)
+  plt.show()
+
+
+  fig, ax = plt.subplots(1, 2)
+  b = net(test)
   a = b[0]
   a = a.permute(1, 2, 0)
   a = a.cpu().data.numpy()
+  if not norm:
+    a = a.astype(np.int)
+  ax[0].imshow(a)
 
+  test = test[0].permute(1,2,0)
+  test = test.cpu().data.numpy()
+  ax[1].imshow(test)
 
-  plt.imshow(a)
+  k = net.subsample(x[15:16])
+  ll = net.criterion(b, k)
+  print(ll)
   plt.show()
