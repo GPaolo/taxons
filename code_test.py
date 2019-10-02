661d37ed37ddeba8880d517dfc59a17fe56d3c63
Giuseppe Laptop <giuseppe.paolo93@gmail.com>
2019-10-02 11:43:43 +0200

    Doxygen and deap

--- code_test.py
+++ code_test.py
@@ -1,26 +1,47 @@
 # Created by Giuseppe Paolo 
 # Date: 26/03/19
 
-import gym, gym_fastsim, pybulletgym
-import numpy
-import matplotlib.pyplot as plt
+import numpy as np
+import deap
+from deap import base, creator, tools
 
-env = gym.make("AntMuJoCoEnv-v0")
-env.render()
-obs = env.reset()
-print(obs)
+class BaseAgent(object):
 
-plt.figure()
+  def __init__(self, mutation_distr=None, **kwargs):
+    '''
+    This class defines the base agent from which other agents should inherit
+    '''
+    if mutation_distr is None:
+      # Define normal distr with sigma and mu
+      self.sigma = 0.05
+      self.mu = 0.
+      def normal(*args):
+        return self.sigma * np.random.randn(*args) + self.mu
+
+      self.mutation_operator = normal
+    else:
+      self.mutation_operator = mutation_distr
+    self.action_len = 0.
+    self._genome = []
+
+  def print(self):
+    print(0)
+
+creator.create("FitnessMin", base.Fitness, weights=(-1.0, ))
+creator.create('Individual', BaseAgent, mutation_distribution=None, fitness=creator.FitnessMin)
+
+POP_SIZE = 3
+toolbox = base.Toolbox()
+toolbox.register("individual", creator.Individual)
+toolbox.register("select", tools.selTournament, tournsize=3)
 
 
-for k in range(2000):
-  o, r, eo, info = env.step(env.action_space.sample())
-  env.render()
-  # plt.draw()
-  print(env.robot.body_xyz)
-image = env.render(mode="rgb_array", top_bottom=True)
-print(image.dtype)
-plt.imshow(image)
-plt.show()
+# toolbox.register("population", tools.initRepeat, list, creator.Individual)
+#
+# pop = toolbox.population(n=POP_SIZE)
+# archive = toolbox.population(n=0)
+ind = toolbox.individual()
+print(ind)
+print(ind.fitness.valid)
 
 
